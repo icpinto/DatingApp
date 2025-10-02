55d45e4248ca26eb22aca6a72f967f6562483b2f
icpinto <36252590+icpinto@users.noreply.github.com>
2025-10-02 19:18:06 +0100

    Consolidate core schema migration

--- internals/db/migrations/001_init_core_schema.sql
+++ internals/db/migrations/001_init_core_schema.sql
@@ -9,6 +9,7 @@ BEGIN;
 DROP TRIGGER IF EXISTS trg_profiles_set_updated_at ON profiles;
 DROP FUNCTION IF EXISTS set_profiles_updated_at();
 
+DROP TABLE IF EXISTS conversations CASCADE;
 DROP TABLE IF EXISTS conversation_outbox CASCADE;
 DROP TABLE IF EXISTS friend_requests CASCADE;
 DROP TABLE IF EXISTS profiles CASCADE;
